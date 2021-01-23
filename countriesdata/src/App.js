ba4b83c888a7fce3a07530f8eb8a645429240cc5
neldeles <neldeles@gmail.com>
2021-01-24 06:45:02 +0800

    Exercise 2.13 completed.

--- countriesdata/src/App.js
+++ countriesdata/src/App.js
@@ -1,58 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-
-const DisplayCountries = ({ countries, newFilter }) => {
-  const countriesToShow =
-    countries.filter(country =>
-      country.name.toLowerCase()
-        .includes(
-          newFilter.toLowerCase()
-        )
-    )
-
-  if (countriesToShow.length > 10) {
-    return (
-      <p>
-        Too many matches, make search term more specific
-      </p>
-    )
-  } else if (countriesToShow.length === 1) {
-    return (
-      <div>
-        {countriesToShow.map(country =>
-          <div key={country.name}>
-            <h1>{country.name}</h1>
-            <p>{country.capital}</p>
-            <p>{country.population}</p>
-            <h2>languages</h2>
-            <ul>
-              {country.languages.map(lang =>
-                <li key={lang.name}>
-                  {lang.name}
-                </li>
-              )}
-            </ul>
-            <img
-              src={country.flag}
-              alt="new"
-            />
-          </div>
-        )}
-      </div>
-    )
-  } else {
-    return (
-      <div>
-        {countriesToShow.map(country =>
-          <p key={country.name}>
-            {country.name}
-          </p>
-        )}
-      </div>
-    )
-  }
-
-}
+import DisplayCountries from './components/DisplayCountries'
 
 const App = () => {
   // state for the countries object
@@ -82,6 +30,7 @@ const App = () => {
       <DisplayCountries
         countries={countries}
         newFilter={newFilter}
+        handleShowClick={setNewFilter}
       />
     </div>
   );
---
+++ countriesdata/src/components/DisplayCountries.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const DisplayCountries = ({ countries, newFilter, handleShowClick }) => {
+  const countriesToShow =
+    countries.filter(country =>
+      country.name.toLowerCase()
+        .includes(
+          newFilter.toLowerCase()
+        )
+    )
+
+  if (countriesToShow.length > 10) {
+    return (
+      <p>
+        Too many matches, make search term more specific
+      </p>
+    )
+  } else if (countriesToShow.length === 1) {
+    return (
+      <div>
+        {countriesToShow.map(country =>
+          <div key={country.name}>
+            <h1>{country.name}</h1>
+            <p>{country.capital}</p>
+            <p>{country.population}</p>
+            <h2>languages</h2>
+            <ul>
+              {country.languages.map(lang =>
+                <li key={lang.name}>
+                  {lang.name}
+                </li>
+              )}
+            </ul>
+            <img
+              src={country.flag}
+              alt="new"
+            />
+          </div>
+        )}
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        {countriesToShow.map(country =>
+          <p key={country.name}>
+            {country.name}
+            <button onClick={() => handleShowClick(country.name)}>show</button>
+          </p>
+        )}
+      </div>
+    )
+  }
+}
+
+export default DisplayCountries
